98a7c5303b4af9ff06b2db4535c5b8cf0ac0db11
MdImranDev <67498399+MdImranDev@users.noreply.github.com>
2020-07-28 23:47:31 +0530

    Add files via upload

---
+++ css file/animation+counter.js
@@ -0,0 +1,12 @@
+//$(".num123").counterUp({time:2000});
+$('.num123').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 5000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
